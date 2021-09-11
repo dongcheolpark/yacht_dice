47940ad2c04d36f792d4a5c6ba46658e07df73f6
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-09-11 20:06:46 +0900

    init

---
+++ src/front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <MainTitle/>
+</template>
+
+<script>
+import MainTitle from './components/MainTitle.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainTitle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/front/src/components/MainTitle.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+	<h1>MainTitle</h1>
+	<input @keyup.enter="RoomNumCheck" v-model="RoomNum" placeholder="1~9999까지 입력하세요"> 
+	<p v-if="ErrorShow">{{RoomNum}}</p>
+	<button v-on:click="RoomNumCheck">submit</button>
+</div>
+</template>
+
+<script>
+export default {
+	name : 'MainTitle',
+	data() {
+		return {
+			msg : "hello",
+			RoomNum : 0,
+			ErrorShow : false,
+		}
+	},
+	method : {
+		RoomNumCheck() {
+			this.msg = "에러발생";
+			this.ErrorShow = true;
+		},
+	}
+}
+</script>
